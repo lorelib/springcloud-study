385e2c34820c8633604d08a4e2a63b68d4f25bd9
listening <893979653@qq.com>
2016-12-28 20:32:03 +0800

    编写服务提供者

--- provider-user/src/main/java/com/lorelib/springcloud/provider/user/controller/UserController.java
+++ provider-user/src/main/java/com/lorelib/springcloud/provider/user/controller/UserController.java
@@ -7,12 +7,14 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by listening on 2016/12/28.
  */
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private DiscoveryClient discoveryClient;
