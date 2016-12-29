1d2b104d4aa71bd3143f88c4603756c7f1ec748d
lorelib <893979653@qq.com>
2016-12-29 23:59:18 +0800

    消费服务消费者

---
+++ consumer-movie-ribbon/src/main/java/com/lorelib/springcloud/consumer/movie/ribbon/service/RibbonService.java
@@ -0,0 +1,19 @@
+package com.lorelib.springcloud.consumer.movie.ribbon.service;
+
+import com.lorelib.springcloud.consumer.movie.ribbon.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by listening on 2016/12/29.
+ */
+@Service
+public class RibbonService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public User findById(Long id) {
+        return restTemplate.getForObject("http://provider-user/" + id, User.class);
+    }
+}
