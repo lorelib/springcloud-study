78029dee6c31e4c907d2e0f2f4327e1f272c8da5
listening <893979653@qq.com>
2018-04-23 11:37:17 +0800

    eureka服务发现

---
+++ microservice-discovery/src/main/java/com/lorelib/springcloud/discovery/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.lorelib.springcloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现
+ * @author listening
+ * @date 2018-04-20 17:50
+ * @since v1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+  }
+}
