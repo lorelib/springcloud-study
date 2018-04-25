e245bbf75a4724a6c815f69a6fab03026e805b96
listening <893979653@qq.com>
2018-04-25 18:20:44 +0800

    添加config client

--- microservice-consumer-movie-feign/src/main/java/com/lorelib/springcloud/movie/feign/controller/FeignController.java
+++ microservice-consumer-movie-feign/src/main/java/com/lorelib/springcloud/movie/feign/controller/FeignController.java
@@ -3,6 +3,8 @@ package com.lorelib.springcloud.movie.feign.controller;
 import com.lorelib.springcloud.movie.feign.feign.UserFeignClient;
 import com.lorelib.springcloud.movie.feign.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,20 @@ import javax.annotation.Resource;
  * @since v1.0
  */
 @RestController
+@RefreshScope
 public class FeignController {
   @Resource
   private UserFeignClient userFeignClient;
+  @Value("${hello}")
+  private String hello;
 
   @GetMapping("feign/{id}")
   public User findById(@PathVariable Long id) {
     return userFeignClient.findById(id);
+  }
+
+  @GetMapping("/hello")
+  public String hello() {
+    return hello;
   }
 }
