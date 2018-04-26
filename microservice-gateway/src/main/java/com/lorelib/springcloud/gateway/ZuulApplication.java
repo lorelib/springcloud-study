d81c282f64db59aebbca0a17967d661db2179747
listening <893979653@qq.com>
2018-04-26 17:45:12 +0800

    集成zuul网关

---
+++ microservice-gateway/src/main/java/com/lorelib/springcloud/gateway/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.lorelib.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author listening
+ * @date 2018-04-26 13:01
+ * @since v1.0
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ZuulApplication.class, args);
+  }
+}
