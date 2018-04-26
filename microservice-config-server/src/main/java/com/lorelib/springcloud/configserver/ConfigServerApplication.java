96ea8102e099a3d5fab93d18312bacb8cfb8e88f
listening <893979653@qq.com>
2018-04-26 10:55:33 +0800

    集成注册中心

--- microservice-config-server/src/main/java/com/lorelib/springcloud/configserver/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/lorelib/springcloud/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.lorelib.springcloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(ConfigServerApplication.class, args);
